3b1e571b57f13c6542bacc38f0058bc0d496340b
Aisha Farooque <aishafarooque@users.noreply.github.com>
2020-02-08 13:33:53 -0600

    Add files via upload

---
+++ ListTutorial/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string> {"Brian", "Jamie", "Lee"};
+            // foreach (var name in names)
+            // {
+            // 	Console.WriteLine($"The name is {name}");
+            // }
+
+            names.Add("Maria");
+            names.Remove("Jamie");
+
+            names.Sort();
+            // foreach (var name in names)
+            // {
+            // 	Console.WriteLine($"The name is {name.ToUpper()}");
+            // }            
+
+
+            var fibonacciNumbers = new List<int> {1, 1};
+
+            for (int i = 0; i < 20; i++) {
+	            var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+	            var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+            	fibonacciNumbers.Add(previous + previous2);
+            	Console.WriteLine(fibonacciNumbers[i]);
+            }
+
+
+        }
+    }
+}
---
+++ NumbersInCSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NumbersInCSharp
+{
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+			double radius = 2.5;
+			double area = Math.PI * radius * radius;
+			Console.WriteLine(area);
+		}
+	}
+}
